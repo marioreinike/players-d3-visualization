4effc59b95d491fc7f8a8558a522fbee5c65ceff
mario <mareinike@uc.cl>
2020-10-22 00:55:18 -0300

    functional with order

--- card.js
+++ card.js
@@ -1,5 +1,24 @@
 const radarColors = ['blue', 'red', 'green'];
 const cardColors = ['#ffd700', '#c0c0c0', '#cd7f32']
+const svgSize = 160;
+const svgCenter = svgSize / 2;
+const features = [
+  'PACE',
+  'SHOOTING',
+  'PASSING',
+  'DRIBBLING',
+  'DEFENDING',
+  'PHYSICAL'
+]
+
+const cards = document.getElementById('cards');
+
+async function renderCard(player, index) {
+  const card = await renderCardData(player, index);
+  cards.appendChild(card);
+  await renderSvg(player, index);
+  return card;
+}
 
 function getRadarColor(position) {
   if (['CB', 'RB', 'LB', 'LWB', 'RWB'].includes(position)) {
@@ -16,7 +35,7 @@ function getCardColor(rating) {
   return cardColors[2];
 }
 
-function renderCard(player, i) {
+async function renderCardData(player, i) {
   const card = document.createElement('div');
   card.className = 'card';
   card.style.backgroundColor = getCardColor(player.RATING);
@@ -49,4 +68,84 @@ function renderCard(player, i) {
   return card;
 }
 
-export { renderCard, cardColors, getRadarColor };
+async function renderSvg(player, index) {
+  const scaleValue = d3.scaleLinear()
+    .domain([1, 99])
+    .range([0, 0.35 * svgSize]);
+  
+  const convertAngle = (angle, radius) => {
+    const x = scaleValue(radius) * Math.cos(angle) + svgCenter;
+    const y = scaleValue(radius) * Math.sin(angle) + svgCenter;
+    return { x, y };
+  };
+
+  // CREAMOS LA SELECCION
+  const svg = d3.select(`#circle-${index}`).append('svg');
+
+  // CIRCULO BLANCO
+  svg
+    .attr('height', svgSize)
+    .attr('width', svgSize)
+    .append('circle')
+    .attr('cx', svgCenter)
+    .attr('cy', svgCenter)
+    .attr('fill', 'white')
+    .attr('r', svgSize / 2);
+
+  // PATH DE BORDES
+  const path = d3.path();
+  for (let i = 0; i < features.length; i++) {
+    const angle = (Math.PI / 2) + (i / features.length * 2 * Math.PI);
+    const position = convertAngle(angle, 99);
+    if (i == 0)
+      path.moveTo(position.x, position.y);
+    else
+      path.lineTo(position.x, position.y);
+  }
+  path.closePath();
+  svg.append('path')
+    .attr('d', path)
+    .attr('stroke', 'black')
+    .attr('fill', 'none');
+
+  // EJES
+  features.map((feature, i) => {
+    const angle = (Math.PI / 2) + (i / features.length * 2 * Math.PI);
+    const linePosition = convertAngle(angle, 99);
+    svg.append('line')
+      .attr('x1', svgCenter)
+      .attr('y1', svgCenter)
+      .attr('x2', linePosition.x)
+      .attr('y2', linePosition.y)
+      .attr('stroke', 'black');
+    const labelPosition = convertAngle(angle, 120);
+    svg.append('text')
+      .attr('x', labelPosition.x)
+      .attr('y', labelPosition.y)
+      .text(feature.slice(0, 3))
+      .attr('alignment-baseline', 'middle')
+      .attr('text-anchor', 'middle');
+  });
+
+  // PATH VALORES
+  const createChartPath = (player) => {
+    const path = d3.path();
+    features.map((feature, i) => {
+      const angle = (Math.PI / 2) + (i / features.length * 2 * Math.PI);
+      const position = convertAngle(angle, player[feature]);
+      if (i == 0)
+        path.moveTo(position.x, position.y);
+      else
+        path.lineTo(position.x, position.y);
+    });
+    path.closePath();
+    return path;
+  };
+  svg
+    .append('path')
+    .attr('d', createChartPath(player))
+    .attr('fill', getRadarColor(player.POSITION))
+    .attr('fill-opacity', 0.7);
+}
+
+export { renderCard };
